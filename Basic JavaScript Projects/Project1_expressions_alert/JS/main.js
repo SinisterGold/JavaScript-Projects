0d63bae07d1e25a3d5226a1ea7eac66bb34ab8c7
SinisterGold <128642241+SinisterGold@users.noreply.github.com>
2024-12-13 10:37:07 -0700

    Basic JavaScript Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+
+//VARIABLES
+var A = "Multiple", B = "Variable", C = "Assignment";
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+//VARIABLES END
+
+12+64 // this is an expression
+
+window.alert(Sent1+Sent2); //this makes an alert appear on the screen and it is the concatenion of sent1 and sent2
+
